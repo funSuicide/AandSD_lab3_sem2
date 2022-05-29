3bd6794f6ef7ea8d0a550f6144fae7d35c81bd29
Admin <Admin@LAPTOP-U4DN8FG5>
2022-05-29 11:59:54 +0400

    Добавлен класс "Город" с полями: "Имя города" и "Число жителей". Методы: конструктор с параметрами, печать информации о городе.

--- AandSD_lab3_sem2/City.h
+++ AandSD_lab3_sem2/City.h
@@ -1,1 +1,15 @@
-#pragma once
+#include <string>
+#include <iostream>
+
+class City {
+	std::string nameCity;
+	unsigned int countResidents;
+public:
+	City(const std::string& nameCity, unsigned int countResidents) {
+		this->nameCity = nameCity; 
+		this->countResidents = countResidents;
+	}
+	void PrintCity() const {
+		std::cout << "City: " << nameCity << "(" << countResidents << ")";
+	}
+};
